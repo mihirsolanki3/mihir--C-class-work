6ef1b9abbea1e7c1531bec94ebc8de5338e53b4a
mihirsolanki3 <smihir118@gmail.com>
2022-07-20 23:44:12 +0530

    class work

---
+++ class program/fun1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void fun1()
+{
+	printf("\nFun1 Called.");
+}
+
+void fun2()
+{
+	printf("\nFun2 Called.");
+}
+
+void fun3()
+{
+	printf("\nFun3 Called.");
+}
+
+void main()
+{
+	printf("\nPractical of Functoins.");
+	fun1();
+	fun2();
+	fun3();
+}
